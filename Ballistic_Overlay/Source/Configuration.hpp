dab2439edf3144c9177a3c1755d567b7ee72c961
zencyber <139611438+zencyber0@users.noreply.github.com>
2024-12-23 15:26:20 -0500

    Delete Ballistic_Overlay/Source directory

--- Ballistic_Overlay/Source/Configuration.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-namespace Configuration
-{
-	inline const char* Name = "Ballistic";
-	inline const char* Version = "1.0.0";
-}
--- Ballistic_Overlay/Source/Globals.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <cmath>
-
-struct {
-	int WalkSpeed = 16;
-	int JumpPower = 50;
-	float Gravity = 196.2f;
-} Globals;
